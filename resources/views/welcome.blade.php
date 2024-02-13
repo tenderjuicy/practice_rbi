1f7eb79def7125e55e473b2a58c96c39a91c3e46
unknown <jocelynsumulong09@gmail.com>
2024-02-13 13:44:05 +0800

    Editing the welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -206,6 +206,8 @@
                           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+
                         </div>
                         <div class="carousel-inner" role="listbox">
                           <div class="carousel-item active">
